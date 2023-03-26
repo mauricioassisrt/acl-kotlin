b0af36236ee02d45ad239dc0a9e49a98117270f0
mauricio.notebook <mauricioassisrt@gmail.com>
2023-03-26 10:19:43 -0300

    inicial

---
+++ acl/src/main/kotlin/br/com/acl/acl/AclApplication.kt
@@ -0,0 +1,11 @@
+package br.com.acl.acl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AclApplication
+
+fun main(args: Array<String>) {
+	runApplication<AclApplication>(*args)
+}
---
+++ acl/src/test/kotlin/br/com/acl/acl/AclApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.acl.acl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AclApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
