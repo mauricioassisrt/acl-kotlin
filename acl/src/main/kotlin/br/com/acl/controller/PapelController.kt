ecf5ac0db03a8775db0b609703bb23317750eb3e
mauricio.notebook <mauricioassisrt@gmail.com>
2023-03-28 21:50:29 -0300

    papel

--- acl/src/main/kotlin/br/com/acl/controller/PapelController.kt
+++ acl/src/main/kotlin/br/com/acl/controller/PapelController.kt
@@ -6,11 +6,12 @@ import br.com.acl.extension.toPageResponse
 import br.com.acl.extension.toResponse
 import br.com.acl.service.PapelService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.awt.print.Pageable
+
 
 @RestController
 @RequestMapping("/api/papeis/")
@@ -18,7 +19,7 @@ class PapelController (){
     @Autowired
     private lateinit var papelService: PapelService
     @GetMapping
-    fun findAll(): List<PapelResponse> {
-        return papelService.findAll().map{it.toResponse()}
+    fun findAll(@PageableDefault(page= 0, size = 10)  pageable: Pageable): PageResponse<PapelResponse> {
+        return papelService.findAll(pageable).map{it.toResponse()}.toPageResponse()
     }
 }
--- acl/src/main/kotlin/br/com/acl/service/PapelService.kt
+++ acl/src/main/kotlin/br/com/acl/service/PapelService.kt
@@ -4,14 +4,15 @@ import br.com.acl.model.PapelModel
 import br.com.acl.repository.PapelRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import java.awt.print.Pageable
 
 @Service
-class PapelService() {
+class PapelService {
     @Autowired
     private lateinit var papelRepository: PapelRepository
-    fun findAll(): MutableList<PapelModel> {
-        return papelRepository.findAll()
+
+    fun findAll(pageable: Pageable): Page<PapelModel> {
+        return papelRepository.findAll(pageable)
     }
 }
