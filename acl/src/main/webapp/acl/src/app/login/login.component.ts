38ede9a450a51171084c7a81b17e598f3ea4b96b
mauricio.notebook <mauricioassisrt@gmail.com>
2023-06-11 22:38:01 -0300

    toast com style dinamico

--- acl/src/main/webapp/acl/src/app/login/login.component.ts
+++ acl/src/main/webapp/acl/src/app/login/login.component.ts
@@ -1,31 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {LoginService} from "../auth/services/login.service";
 import {Router} from "@angular/router";
+import {ToastComponent} from "../toast/toast.component";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
- login = {
-   email:"",
-   senha: "",
- }
 
-  constructor(private authService: LoginService, private route: Router) {}
+export class LoginComponent implements OnInit {
+  @ViewChild(ToastComponent) toastComponent!: ToastComponent;
+  tituloError: string = '';
+  menssagemError: string = '';
+  classError: string = '';
+  login = {
+    email: "",
+    senha: "",
+  }
+  loading: boolean = false;
 
-  onSubmit(){
+  constructor(private authService: LoginService,
+              private route: Router) {
+  }
+
+  ngOnInit(): void {
+    if (this.authService.getToken() != null) {
+      this.route.navigate([''])
+    } else {
+      this.route.navigate(['login'])
+    }
+
+  }
+
+  onSubmit() {
+    this.startLoading();
     this.authService.login(this.login.email, this.login.senha).subscribe(
       (response) => {
+        this.stopLoading();
         const token = response.token; // Supondo que sua API retorne um objeto com um token
         this.authService.setToken(token);
         // Redirecionar para outra rota, por exemplo, a página inicial
-        this.route.navigate([''])
+        this.route.navigate(['']);
       },
       (error) => {
-        console.error('Erro no login:', error);
+        this.tituloError = "Falha ao autenticar";
+        this.menssagemError = error.error.message;
+        this.classError = "bg-warning"
+        this.toastComponent.show();
+        this.stopLoading();
       }
     );
   }
+
+
+  startLoading() {
+    this.loading = true;
+  }
+
+  stopLoading() {
+    this.loading = false;
+  }
+
 }
